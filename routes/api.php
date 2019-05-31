420459468afce594a4a8076f540a9c2255d96054
FredPeal <frederickpeal@gmail.com>
2019-05-31 17:28:58 -0400

    docker

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/contactos','ContactosController');
+Route::resource('/contactos', 'ContactosController');
